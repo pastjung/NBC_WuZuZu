b8491497b04c8f7006c4c5b64bc4ee0fd6aa97c5
pastjung <chwogus0303@naver.com>
2024-04-19 09:21:00 +0900

    publish: 테스트 코드 추가

--- src/test/java/com/sparta/wuzuzu/WuzuzuApplicationTests.java
+++ src/test/java/com/sparta/wuzuzu/WuzuzuApplicationTests.java
@@ -3,7 +3,7 @@ package com.sparta.wuzuzu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class WuzuzuApplicationTests {
 
     @Test
